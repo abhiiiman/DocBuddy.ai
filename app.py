dd5fe53e68598a1aa2f7cd4c2a3cf3bd4b98722e
Abhijit Mandal <111262410+abhiiiman@users.noreply.github.com>
2024-07-01 20:39:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,23 @@ warnings.filterwarnings("ignore")
 
 load_dotenv()
 
+# Load Firebase credentials from Streamlit secrets
+firebase_creds = {
+    "type": st.secrets["firebase"]["type"],
+    "project_id": st.secrets["firebase"]["project_id"],
+    "private_key_id": st.secrets["firebase"]["private_key_id"],
+    "private_key": st.secrets["firebase"]["private_key"].replace('\\n', '\n'),
+    "client_email": st.secrets["firebase"]["client_email"],
+    "client_id": st.secrets["firebase"]["client_id"],
+    "auth_uri": st.secrets["firebase"]["auth_uri"],
+    "token_uri": st.secrets["firebase"]["token_uri"],
+    "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"],
+    "universe_domain": st.secrets["firebase"]["universe_domain"]
+}
+
 # init firebase app here.
-cred = credentials.Certificate(st.secrets["FIREBASE_JSON_PATH"])
+cred = credentials.Certificate(firebase_creds)
 try:
     firebase_admin.get_app()
 except ValueError as e:
